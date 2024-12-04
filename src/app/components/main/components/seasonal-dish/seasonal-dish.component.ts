04fb7144e7c2aa764105efd21270949a528d473a
LuaAguirre <renatoag98@gmail.com>
2024-12-04 16:52:44 -0500

    feature: add seasonal-dish

--- src/app/components/main/components/seasonal-dish/seasonal-dish.component.ts
+++ src/app/components/main/components/seasonal-dish/seasonal-dish.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../button/button.component';
 
 @Component({
   selector: 'app-seasonal-dish',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './seasonal-dish.component.html',
   styleUrl: './seasonal-dish.component.css'
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { SeasonalDishComponent } from './components/seasonal-dish/seasonal-dish.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RecipesComponent],
+  imports: [RecipesComponent, SeasonalDishComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
