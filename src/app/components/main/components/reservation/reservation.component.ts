df42f9e9c76a489a74e63455d1d97e944e8c568b
LuaAguirre <renatoag98@gmail.com>
2024-12-04 22:33:06 -0500

    feature: add reservation

--- src/app/components/main/components/reservation/reservation.component.ts
+++ src/app/components/main/components/reservation/reservation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../button/button.component';
 
 @Component({
   selector: 'app-reservation',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.css'
 })
