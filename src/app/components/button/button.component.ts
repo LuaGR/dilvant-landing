f4dc4ca900a2ffb3dbaa32232e7506848d6a16e5
LuaAguirre <renatoag98@gmail.com>
2024-12-04 23:50:01 -0500

    style: fix buttons

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -11,4 +11,8 @@ export class ButtonComponent {
   text = input('')
   backgroundColor = input('')
   fontColor = input('')
+  padding = input('')
+  fontSize = input('')
+  fontWeight = input('')
+  border = input('')
 }
